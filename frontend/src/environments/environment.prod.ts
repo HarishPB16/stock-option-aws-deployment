b1a41c28dcd8ef7560cc1c601e22aedfc0d6948b
harish bansode <harishpbansode16@gmail.com>
2026-03-18 12:00:47 +0530

    Updated the prod backedn url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: '/api/v1' // Assumes same domain serving or reverse proxied through Nginx on ECS
+    apiUrl: 'https://b3pbays3fv.ap-south-1.awsapprunner.com/api/v1' // Assumes same domain serving or reverse proxied through Nginx on ECS
 };
