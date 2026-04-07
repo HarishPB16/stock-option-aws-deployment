e8b5cbc75d28ccd597964cd211e388899498b1a2
harish bansode <harishpbansode16@gmail.com>
2026-04-07 20:47:03 +0530

    feat: add Angular Service Worker support and PWA configuration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 // Shared/Layout modules can go here
 
 @NgModule({
@@ -15,7 +16,13 @@ import { CoreModule } from './core/core.module';
         BrowserModule,
         BrowserAnimationsModule,
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
